249706e37b396cadbd94ef25f1aa4e0299dace46
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-11 12:53:54 +0300

    .

--- Task 4.2/Program.cs
+++ Task 4.2/Program.cs
@@ -8,8 +8,29 @@ namespace Task_4._2
 {
     class Program
     {
+        private static void Rec(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write("{0} ", n);
+                Rec(n - 1);
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите натуральное число: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            if (n == 0)
+                Console.WriteLine("Ошибка: N = 0");
+            else if (n < 0)
+                Console.WriteLine("Ошибка: N меньше 0");
+            else if (n > 1000)
+                Console.WriteLine("Ошибка: N больше 1000");
+            else
+                Rec(n);
+
+            Console.ReadKey();
         }
     }
 }
